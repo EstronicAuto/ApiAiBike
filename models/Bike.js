049e05806d9f6dcf7df29c0aca4a6a55f5c713c6
EstronicAuto <estronic.automa@gmail.com>
2023-06-05 10:28:24 -0300

    nome errado

--- models/Bike.js
+++ models/Bike.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const BikeSchema = mongoose.Schema({
 
-    numberSerir :{
+    numberSerie :{
         type: String,
         require: true,
         default: 0
